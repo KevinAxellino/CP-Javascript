440ffea86829cc5231219d1bc31617d0a330f8f4
KevinAxellino <kevinaxellino@gmail.com>
2024-09-02 22:22:52 +0700

    day2

---
+++ 3.descendingOrder.js
@@ -0,0 +1,12 @@
+https://www.codewars.com/kata/5467e4d82edf8bbf40000155/train/javascript
+
+function descendingOrder(n){
+    let arr = [];
+    while(Math.floor(n) / 10 != 0){
+        arr.push(Math.floor(n%10));
+        n /= 10;
+    }
+    return Number(arr.sort((a,b) => b - a).join('')) ;
+  }
+
+  console.log(descendingOrder()); //5
